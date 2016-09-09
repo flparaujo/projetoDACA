30e3d39ac1146f9abf8dc2bc5a940415eff130ba
Felipe Araujo <felipe.andrade@ccc.ufcg.edu.br>
2016-09-09 14:17:23 -0300

    tentando fazer o teste cadastrar usuário

---
+++ src/test/java/dirlididi/UsuarioTest.java
@@ -0,0 +1,73 @@
+package dirlididi;
+
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static com.jayway.restassured.RestAssured.*;
+
+import dirlididi.domain.*;
+import dirlididi.repositories.NormalRepository;
+
+import com.google.gson.*;
+import com.jayway.restassured.http.ContentType;
+
+@SpringApplicationConfiguration(classes = DirlididApplication.class)
+@WebIntegrationTest("server.port=0")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UsuarioTest {
+	
+	@Value("${local.server.port}")
+    private int port;
+    
+    @Autowired
+    private NormalRepository normalRepository;
+    
+    private Normal usuarioNormal;
+    
+    @Before
+    public void setup(){
+        usuarioNormal = new Normal("usuario.normal@gmail.com", "99999999");
+        normalRepository.save(usuarioNormal);
+    }
+
+    @After
+    public void tearDown() {
+        normalRepository.deleteAll();
+    }
+    
+    @Test
+    public void testCadastrarUsuario() throws Exception {
+        Gson gson = new Gson();
+        Normal usuarioNormal = new Normal("usuarioanormal@gmail.com", "000000000000");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(gson.toJson(usuarioNormal))
+                .when()
+                .port(this.port)
+                .post("/api/user")
+                .then().assertThat()
+                .body("email", Matchers.is(usuarioNormal.getEmail()));
+    }
+
+    @Test
+    public void testGetUsuarios() {
+        given().when()
+                .port(this.port)
+                .get("/api/user")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+    
+    @Test
+    public void testGetUsuarioPeloID() {
+    	
+    }
+}
